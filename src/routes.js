c1544b197effbbfd923fba82b9fb39e874859014
Gilberto Espinoso <gilbertosilvaespinosa@gmail.com>
2024-02-13 18:10:01 -0300

    scroll to top

--- src/routes.js
+++ src/routes.js
@@ -6,18 +6,20 @@ import Rodape from "componentes/rodape";
 import PaginaPadrao from "componentes/paginaPadrao";
 import Post from "paginas/post";
 import NaoEncontrada from "paginas/naoEncontrada";
+import ScrollToTop from "componentes/scrollToTop";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Menu />
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />} />
           <Route path="sobremim" element={<SobreMim />} />
-          <Route path="posts/:id" element={<Post />} />
         </Route>
 
+        <Route path="posts/:id" element={<Post />} />
         <Route path="*" element={<NaoEncontrada />} />
       </Routes>
 
